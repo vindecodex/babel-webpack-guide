dc50e3360d94d4185da9b3dace9db83b014aebbe
Vincent Villaluna <villalunavincent@gmail.com>
2019-11-20 10:34:24 +0800

    javascript source codes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {
+  add,
+  subtract,
+  multiply,
+  divide,
+  mod
+} from './math'
+
+console.log("Add: " + (add(5,5)))
+console.log("Subtract: " + (subtract(10,5)))
+console.log("Multiply: " + (multiply(5,5)))
+console.log("Divide: " + (divide(25,5)))
+console.log("Mod: " + (mod(7,3)))
